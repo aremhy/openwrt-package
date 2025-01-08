b36f36048565f102a245b4a2e35cb4e5bce86f04
aremhy <aremhy@qq.com>
2025-01-08 21:22:49 +0800

    内网测速

--- homebox/web/src/cases/ping.ts
+++
@@ -1,9 +0,0 @@
-import { BASE_URL } from '../const'
-
-export const ping = async () => {
-  const now = performance.now()
-  const resp = await fetch(`${BASE_URL}/ping`, { method: 'GET' })
-  await resp.text()
-  const time = performance.now() - now
-  return time
-}
